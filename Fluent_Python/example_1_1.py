be9d116661a1fe1655ac0212ccf5a878d29bddc5
Guangyu He <fredhgy2010@gmail.com>
2022-12-18 14:50:34 +0100

    fluent python: example 1.1

---
+++ Fluent_Python/example_1_1.py
@@ -0,0 +1,30 @@
+"""
+example usage:
+>>> a_deck = FrenchDeck()
+>>> len(a_deck)
+52
+"""
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = ['♣', '♠', '♥', '♦']
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
